9b60147ae745821447e7bb6d71902759104d54da
Alejandro Quiñones Caicedo <50637445+WholeMuffin@users.noreply.github.com>
2021-11-19 20:03:00 -0500

    Se crean componentes
    Se agrega el componente weatherform y weatherinfo

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,18 @@
 import React, {Component} from "react";
+import WeatherInfo from "./components/WeatherInfo";
+import WeatherForm from "./components/WeatherForm";
 
 class App extends Component{
     render(){
         return(
-            <h1>Clima</h1>
+            <div className="container p-4">
+                <div className="row">
+                    <div className="col-md-6">
+                        <WeatherForm />
+                        <WeatherInfo />
+                    </div>
+                </div>
+            </div>
         )
     }
 };
