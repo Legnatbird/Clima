bc49015903a75a5a79e5bd2319199cc97b7e3029
Alejandro Quiñones Caicedo <50637445+WholeMuffin@users.noreply.github.com>
2021-11-19 21:19:41 -0500

    API request
    Se hace la consulta a la web y se obtiene el json

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,19 @@
 import React, {Component} from "react";
 import WeatherInfo from "./components/WeatherInfo";
 import WeatherForm from "./components/WeatherForm";
+import {WEATHER_KEY} from "./keys";
 
 class App extends Component{
-    getWeather = e=>{
+    getWeather = async e => {
         e.preventDefault();
         const {city,country} =  e.target.elements;
         const cityValue = city.value;
         const countryValue = country.value;
+
+        const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}`;
+        const response =  await fetch(API_URL);
+        const data = await response.json();
+        console.log(data);
     }
     render(){
         return(
